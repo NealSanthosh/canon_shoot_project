3cae91c7206bde1b423aa4f083f29f100c5d5edf
SansNeal <62467577+SansNeal@users.noreply.github.com>
2020-04-19 21:38:36 +0530

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,16 @@
+class Ball {
+  constructor(x,y,radius) {
+    var options ={
+        frictionAir: 0,
+        friction: 0,
+        frictionStatic: 1,
+        inertia: Infinity,
+        restitution: 1.0
+    }
+    
+  }
+
+  display() {
+
+  }
+}
